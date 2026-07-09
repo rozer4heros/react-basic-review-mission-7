634dd8deaefc9243bf1fccbfaf11572511c61029
rozer4heros <rozer4heros@gmail.com>
2026-07-09 16:51:55 +0900

    feat: update App component to display data from JSON and enhance heading styles
    미션 1. data.json 불러오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import "./App.css";
+import reactData from "./data/data.json";
 
 function App() {
+  console.log(reactData);
+
   return (
     <>
-      <></>
+      <h1>react basic review mission 2</h1>
+      <p>전체 학습 항목 수: {reactData.length}개</p>
     </>
   );
 }
